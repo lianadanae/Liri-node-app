f4c3c8be7c639a2025eb3bd2a008c9268e8ff567
lianadanae <lianabette@gmail.com>
2019-10-15 16:12:52 -0400

    bands-in-town works

--- liri.js
+++ liri.js
@@ -1,28 +1,28 @@
 require("dotenv").config();
 //Variables
-//var request = require("request");
+var request = require("request");
 var Spotify = require("node-spotify-api");
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
-//var moment = require("moment");
+var Moment = require("moment");
 //var fs = require("fs");
-//var axios = require("axios");
+var Axios = require("axios");
 
 //Variable's for user inputs
 let command = process.argv[2];
 let input = process.argv[3];
 
 //Switches for user actions
-switch (command){
-    
+switch (command) {
+
     case "spotify-this-song":
         spotifyThisSong();
         break;
-    
+
     case "concert-this":
         bandInfo();
         break;
-    
+
     case "movie-this":
         movieInfo();
         break;
@@ -32,26 +32,47 @@ switch (command){
         break;
     default:
 
-    break;
-    }
+        break;
+}
+
+//Spotify Code
 
 function spotifyThisSong() {
     if (!input) {
         input = "The Sign Ace of Base"
     }
 }
-spotify.search({ type: "track", query: input },
-  function (err, data) {
-      if (err) {
-          return console.log("Error occured: " + err);
-      }
+spotify.search({
+        type: "track",
+        query: input
+    },
+    function (err, data) {
+        if (err) {
+            return console.log("Error occured: " + err);
+        }
 
-  var songInfo = data.tracks.items[0];
-    console.log("------------------------------")
-    console.log("Artist: " + songInfo.artists[0].name);
-    console.log("Song: " + songInfo.name);
-    console.log("Preview Link: " + songInfo.preview_url);
-    console.log("Album: " + songInfo.album.name);
-    console.log("------------------------------")
-});
+        var songInfo = data.tracks.items[0];
+        console.log("------------------------------")
+        console.log("Artist: " + songInfo.artists[0].name);
+        console.log("Song: " + songInfo.name);
+        console.log("Preview Link: " + songInfo.preview_url);
+        console.log("Album: " + songInfo.album.name);
+        console.log("------------------------------")
+    });
 
+//Bands in Town Code
+
+function bandInfo() {
+
+    Axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
+        .then(function (response) {
+
+            console.log("------------------------------");
+            console.log("Venue Name: " + response.data[1].venue.name);
+            console.log("Venue Location: " + response.data[1].venue.city);
+            console.log("Event Date: " + Moment(response.data[1].datetime).format("MM/DD/YYYY"));
+            console.log("------------------------------");
+
+        });
+
+}
